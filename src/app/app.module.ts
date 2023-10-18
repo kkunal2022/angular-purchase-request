4f9d8143f71b5ea5ca574b29841e2f1685c0367e
kkunal2022 <84966950+kkunal2022@users.noreply.github.com>
2023-10-18 22:41:04 +0300

    Date Picker Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MomentDateModule } from '@angular/material-moment-adapter';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatInputModule } from '@angular/material/input'
+import {
+  MomentDateModule,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter'
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  NativeDateModule,
+} from '@angular/material/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './purchaserequest/list.component';
-import { ProductViewComponent } from './purchaserequest/view.component';
-import { ProductAddEditComponent } from './purchaserequest/add-edit.component';
-import { ProductService } from './_services/product.service';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ProductListComponent } from './purchaserequest/list.component'
+import { ProductViewComponent } from './purchaserequest/view.component'
+import { ProductAddEditComponent } from './purchaserequest/add-edit.component'
+import { ProductService } from './_services/product.service'
+
+export const APP_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+}
 
 @NgModule({
   declarations: [
@@ -31,8 +54,18 @@ import { ProductService } from './_services/product.service';
     MatDatepickerModule,
     MatInputModule,
     MomentDateModule,
+    NativeDateModule,
+    BrowserAnimationsModule,
   ],
-  providers: [ProductService],
+  providers: [
+    ProductService,
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
